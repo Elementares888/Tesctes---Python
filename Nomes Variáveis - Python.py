d66009bddb518c43c7a5e7dd7f0aaecf3c1e8ba6
Elementares888 <pedroisraelcarvalho3@gmail.com>
2026-06-02 23:07:30 -0300

    Add files via upload

---
+++ Nomes Variáveis - Python.py
@@ -0,0 +1,34 @@
+print ("Hello World!")
+
+aluno = "Ada Lovelace"
+pediodo_corrente = 10
+ira = 9.99 #valor que define IRA
+laurea = True
+
+aluno2 = "Pedro Israel Carvalho da Silva"
+
+
+# Nomes que não é permitido declarar variáveis:
+# and     as        assert      break       class       continue    def
+# del     elif      else        except      False       finally     for
+# from    global    if          import      in          is          lambda
+# None    nonlocal  not         or          pass        raise       return
+# True    try       while       with        yield
+
+# Uso da função tupe()
+
+exemplo = "Ada Lovelace"
+print(type(exemplo)) #Imprime <class 'str'>
+
+'''Ada despertou uma admiração pelos trabalhos desenvolvidos por é
+Charles Babbage que resultou no interesse de ser sua alma. Babbage
+considerado o projetista do primeiro computador de uso geral.'''
+
+exemplo = 10
+print(type(exemplo)) #Imprime <class 'int'>
+
+exemplo = 9.99
+print(type(exemplo)) #Imprime <class 'float'>
+
+exemplo = True
+print(type(exemplo)) #Imprime <class 'bool'>
