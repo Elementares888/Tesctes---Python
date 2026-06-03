eedad1fe4eb3f6dbf2b7a619d5c67aaa78e45d83
Elementares888 <pedroisraelcarvalho3@gmail.com>
2026-06-02 23:06:41 -0300

    Add files via upload

---
+++ Python - criação.py
@@ -0,0 +1,31 @@
+print("Olá, Python!")
+
+print(10, 3.14)
+
+mensagem = "Olá, Python!"
+
+print(mensagem)
+
+conversa = "Olá, Python!"
+
+speak = "10 /2 = 5"
+
+print(conversa)
+print(speak)
+
+idade = "45"
+
+print(idade)
+
+idade_texto = "30"
+idade_número = 10
+
+resultado_soma = int(idade_texto) + idade_número
+print(resultado_soma)
+
+idade_texto = "30"
+idade_número = 10
+
+resultado_texto = idade_texto + str(idade_número)
+print(resultado_texto)
+
